a8d348900aa03f84334b41278b994d2562b55304
yogi245 <73364333+yogi245@users.noreply.github.com>
2022-09-25 07:35:53 -0500

    holaaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tarea 3 - Grupo 2 ';
+  title = 'Tarea 3 - Grupo 2 er';
 }
