60c603568495db19e59ebb8ec0eb20b391468886
yogi245 <73364333+yogi245@users.noreply.github.com>
2022-09-25 07:46:49 -0500

    comienzol de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tarea 3 - Grupo 2sddr';
+  title = 'Tarea 3 - Grupo 2sr';
 }
