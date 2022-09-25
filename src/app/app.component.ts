dd2e1b6f465f0c64fca9d30158d774659b725e27
yogi245 <73364333+yogi245@users.noreply.github.com>
2022-09-25 07:52:53 -0500

    cambio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tarea 3 - Grupo 2sr';
+  title = 'Tarea 3 - Grupo 2r';
 }
